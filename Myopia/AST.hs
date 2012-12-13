2178bb27182f0310da27310018a1295edafbd9ef
Miikka Koskinen <miikka.koskinen@iki.fi>
2012-12-13 14:06:21 +0200

    Add basic memoization of funcalls

--- Myopia/AST.hs
+++ Myopia/AST.hs
@@ -2,10 +2,10 @@
 module Myopia.AST where
 
 import           Control.Applicative
-import           Control.Monad.Reader
-import           Data.Map             (Map)
-import qualified Data.Map             as M
-import           Data.Monoid
+import           Control.Monad.RWS
+import           Data.Map            (Map)
+import qualified Data.Map            as M
+import qualified Data.Map.Strict     as SM
 
 import           Debug.Trace
 
@@ -31,7 +31,7 @@ instance Monoid Program where
     mempty = Program mempty mempty
     mappend (Program a1 b1) (Program a2 b2) = Program (a1 <> a2) (b1 <> b2)
 
-type MyopiaM = Reader Program
+type MyopiaM = RWS Program () (SM.Map (FunName, [Integer]) Integer)
 
 arityM :: Expr -> MyopiaM Integer
 arityM Z = return 1
@@ -46,7 +46,7 @@ getDef :: FunName -> MyopiaM Expr
 getDef fn = asks ((M.! fn) . funDefs)
 
 runMyopiaM :: MyopiaM a -> Program -> a
-runMyopiaM = runReader
+runMyopiaM f prog = fst $ evalRWS f prog M.empty
 
 getArity :: Program -> FunName -> Integer
 getArity prog fn = runMyopiaM (getDef fn >>= arityM) prog
@@ -57,16 +57,21 @@ eval e p | trace ("eval " ++ show e ++ " [" ++ show (head p) ++ ",…]" ) False 
 eval Z [_] = return 0
 eval S [x] = return $ x + 1
 eval (I i _) xs = return $ xs !! (i - 1)
-eval (C h gs) xs = do --eval h (mapM (`eval` xs) gs)
-    args <- mapM (`eval` xs) gs
-    eval h args
+eval (C h gs) xs = mapM (`eval` xs) gs >>= eval h
 eval (P g _) (0:xs) = eval g xs
 eval (P g h) ((pred -> y):xs) = do
     rec <- eval (P g h) (y:xs)
-    eval h (y : rec :xs)
+    eval h (y : rec : xs)
 eval (M f) xs = minimize f xs 0
-eval (FC "bottom") xs = error "Function \"bottom\" called."
-eval (FC fn) xs = getDef fn >>= \f -> eval f xs
+eval (FC "bottom") _xs = error "Function \"bottom\" called."
+eval (FC fn) xs = do
+    memoizedValue <- gets (SM.lookup (fn, xs))
+    case memoizedValue of
+        Just value -> return value
+        Nothing -> do
+            value <- getDef fn >>= flip eval xs
+            modify $ M.insert (fn, xs) value
+            return value
 
 runProgram :: Program -> FunName -> [Integer] -> Integer
 runProgram prog fn params = runMyopiaM (eval (funDefs prog M.! fn) params) prog
@@ -75,3 +80,5 @@ minimize :: Expr -> [Integer] -> Integer -> MyopiaM Integer
 minimize f xs z = do
     yz <- eval f (z:xs)
     if yz == 0 then return z else minimize f xs (z+1)
+
+-- vim: set ts=4 sw=4 et
