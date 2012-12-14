bd32e1c54fb7a0088a1836c3ea70e2833a8611a1
Miikka Koskinen <miikka.koskinen@iki.fi>
2012-12-14 16:22:39 +0200

    Typecheck all functions always.

--- Myopia.hs
+++ Myopia.hs
@@ -1,50 +1,18 @@
 import           Control.Applicative
 import           Control.Monad
-import           Control.Monad.Trans
-import           Control.Monad.Trans.Either
-import           Data.Map.Lazy              (Map)
-import qualified Data.Map.Lazy              as M
+import qualified Data.Map.Lazy       as M
 import           System.Environment
-import           Text.Printf
 
 import           Myopia.AST
-import           Myopia.Parser              (parseFile)
+import           Myopia.Parser       (parseFile)
 import           Myopia.REPL
-
-type TypeError = (FunName, Expr, String)
-
-ensure :: Bool -> String -> Expr -> String -> EitherT TypeError MyopiaM ()
-ensure p ctx e msg = unless p $ left (ctx, e, msg)
-
-checkM :: FunName -> Expr -> EitherT TypeError MyopiaM ()
-checkM ctx e@(C h gs) = do
-    checkM ctx h
-    mapM_ (checkM ctx) gs
-    a <- fromIntegral <$> lift (arityM h)
-    res <- lift $ mapM arityM gs
-    ensure (length gs == a) ctx e $ printf "Got %d inner functions, %d expected." (length gs) a
-    ensure (all (== head res) res) ctx e $ printf "All inner functions should have equal arity. Got: %s" (show res)
-checkM ctx e@(P g h) = do
-    checkM ctx g
-    checkM ctx h
-    ag <- lift $ arityM g
-    ah <- lift $ arityM h
-    ensure (ah == ag + 2) ctx e $ printf "Arity of case n+1 should be (2 + arity of case 0). Was: %d, %d." ag ah
-checkM ctx e@(M f) = do
-    checkM ctx f
-    x <- lift $ arityM f
-    ensure (x > 1) ctx e $ printf "Arity of minimized function should be at least 2. Got: %d" x
-checkM _ (FC n) = lift (getDef n) >>= checkM n
-checkM _ _ = return ()
+import           Myopia.TypeCheck
 
 
 main :: IO ()
 main = do
     (fp : name : _) <- getArgs
     if fp == "repl" then repl else runFile fp name
-
-traverseWithKey_ :: Applicative t => (k -> a -> t ()) -> Map k a -> t ()
-traverseWithKey_ f m = M.traverseWithKey (\k a -> f k a *> pure a) m *> pure ()
 
 printError :: TypeError -> IO ()
 printError (ctx, e, msg) = do
@@ -55,18 +23,16 @@ printError (ctx, e, msg) = do
 runFile :: FilePath -> String -> IO ()
 runFile fp name = do
     prog <- parseFile fp
-    let def = funDefs prog M.! name
-        mainArity = runMyopiaM (arityM def) prog
-    case runMyopiaM (runEitherT (checkM name def)) prog of
-        Left e   -> printError e
-        Right () -> do
-            traverseWithKey_ (checkTypeDef prog) (typeDefs prog)
-            putStrLn $ "Expecting " ++ show mainArity ++ " parameters."
-            params <- forM [1..mainArity] (\_ -> liftM read getLine)
-            print $ runProgram prog name params
+    case M.lookup name (funDefs prog) of
+        Nothing -> error $ "No such function: " ++ name
+        Just def -> do
+            let mainArity = runMyopiaM (arityM def) prog
+            case typeCheck prog of
+                Left e   -> printError e
+                Right () -> do
+                    putStrLn $ "Expecting " ++ show mainArity ++ " parameters."
+                    params <- forM [1..mainArity] (\_ -> liftM read getLine)
+                    print $ runProgram prog name params
 
-checkTypeDef :: Program -> FunName -> Arity -> IO ()
-checkTypeDef prog fn a =
-    unless (runMyopiaM (arityM (funDefs prog M.! fn)) prog == a) $ error "Typechecking failed."
 
 -- vim: set ts=4 sw=4 et
---
+++ Myopia/TypeCheck.hs
@@ -0,0 +1,65 @@
+module Myopia.TypeCheck where
+
+import           Control.Applicative
+import           Control.Monad              (unless)
+import           Control.Monad.RWS          (asks)
+import           Control.Monad.Trans        (lift)
+import           Control.Monad.Trans.Either (EitherT (..), left)
+import           Data.Map.Lazy              (Map)
+import qualified Data.Map.Lazy              as M
+import           Text.Printf                (printf)
+
+import           Myopia.AST
+
+type TypeError = (FunName, Expr, String)
+type TypeCheckM = EitherT TypeError MyopiaM
+
+ensure :: Bool -> String -> Expr -> String -> TypeCheckM ()
+ensure p ctx e msg = unless p $ left (ctx, e, msg)
+
+checkArity :: FunName -> Expr -> TypeCheckM ()
+checkArity ctx e@(C h gs) = do
+    checkArity ctx h
+    mapM_ (checkArity ctx) gs
+    a <- fromIntegral <$> lift (arityM h)
+    res <- lift $ mapM arityM gs
+    ensure (length gs == a) ctx e $ printf "Got %d inner functions, %d expected." (length gs) a
+    ensure (all (== head res) res) ctx e $ printf "All inner functions should have equal arity. Got: %s" (show res)
+checkArity ctx e@(P g h) = do
+    checkArity ctx g
+    checkArity ctx h
+    ag <- lift $ arityM g
+    ah <- lift $ arityM h
+    ensure (ah == ag + 2) ctx e $ printf "Arity of case n+1 should be (2 + arity of case 0). Was: %d, %d." ag ah
+checkArity ctx e@(M f) = do
+    checkArity ctx f
+    x <- lift $ arityM f
+    ensure (x > 1) ctx e $ printf "Arity of minimized function should be at least 2. Got: %d" x
+checkArity _ (FC n) = lift (getDef n) >>= checkArity n
+checkArity _ _ = return ()
+
+checkTypeDef :: FunName -> Expr -> TypeCheckM ()
+checkTypeDef fn def = do
+    typeDef_ <- lift $ asks $ M.lookup fn . typeDefs
+    case typeDef_ of
+        Nothing -> return ()
+        Just typeDef -> do
+            arity <- lift $ arityM def
+            ensure (arity == typeDef) fn def $ printf "Declared arity %d, inferred arity %d." typeDef arity
+
+checkFunction :: FunName -> TypeCheckM ()
+checkFunction fn = do
+    def <- lift $ getDef fn
+    checkArity   fn def
+    checkTypeDef fn def
+
+runTypeCheckM :: TypeCheckM a -> Program -> Either TypeError a
+runTypeCheckM f = runMyopiaM (runEitherT f)
+
+traverseKeys_ :: Applicative t => (k -> t ()) -> Map k a -> t ()
+traverseKeys_ f m = M.traverseWithKey (\k _ -> f k *> pure ()) m *> pure ()
+
+typeCheck :: Program -> Either TypeError ()
+typeCheck = runTypeCheckM $ lift (asks funDefs) >>= traverseKeys_ checkFunction
+
+-- vim: set ts=4 sw=4 et
